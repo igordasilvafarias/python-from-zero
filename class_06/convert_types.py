048c7dded5a8e6aa2f9bcb8afe3a7a39d668936b
Igor Farias <igordasilvafarias@gmail.com>
2026-02-01 20:59:17 -0300

    feat: add Python type conversion and typecasting examples.

---
+++ class_06/convert_types.py
@@ -0,0 +1,9 @@
+# Type conversion, typecasting, coercion
+# It's the act of converting one type to another
+# immutable and primitive types:
+# str, int, float, bool
+
+print(int('1'), type(int('1')))
+print(type(float('1') + 1))
+print(bool(' '))
+print(str(11) + 'b')
