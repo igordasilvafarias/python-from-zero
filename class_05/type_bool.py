af066689c01f94594d3bc0c92a9f09a220342d78
Igor Farias <igordasilvafarias@gmail.com>
2026-02-01 20:27:23 -0300

    feat: add new file explaining boolean data type and the equality comparison operator.

---
+++ class_05/type_bool.py
@@ -0,0 +1,14 @@
+# Type of data bool (boolean)
+# When questioning something in a program,
+# there are only two possible answers:
+# yes (True) or no (False).
+# There are several operators to "question".
+# Among them the ==, which is a logical operator that
+# questions if a value is equal to another
+
+print(10 == 10)  # Yes => True (True)
+print(10 == 11)  # No => False (False)
+print(type(True))
+print(type(False))
+print(type(10 == 10))
+print(type(10 == 11))
