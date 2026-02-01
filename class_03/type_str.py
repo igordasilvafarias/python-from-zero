ce7be4d6a074dcaec59ac40775c8a7e9f72349a8
Igor Farias <igordasilvafarias@gmail.com>
2026-02-01 14:39:16 -0300

    feat: add Python string type examples in `type_str.py` and update README.

---
+++ class_03/type_str.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Programming language
+Type of typing = Dynamic / Strong
+str -> string -> text
+Strings are texts that are inside quotes
+"""
+print(1234)
+
+# Simple quotes
+print('Igor Farias')
+print(1, 'Igor "Farias"')
+
+# Double quotes
+print("Igor Farias")
+print(2, "Igor 'Farias'")
+
+# Escape
+print("Igor \"Farias\"")
+
+# r
+print(r"Igor \"Farias\"")
