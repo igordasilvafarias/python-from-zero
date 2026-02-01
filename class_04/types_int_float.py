191eec846d41bdb977062a430577ffb8ecf34993
Igor Farias <igordasilvafarias@gmail.com>
2026-02-01 20:17:36 -0300

    feat: Add `types_int_float.py` example demonstrating integer, float types, and the `type()` function.

---
+++ class_04/types_int_float.py
@@ -0,0 +1,20 @@
+# int -> Integer number
+# The int type represents any number
+# positive or negative. int without sign is considered
+# positive.
+# print(11) # int
+# print(-11) # int
+# print(0)
+
+# float -> Number with floating point
+# The float type represents any number
+# positive or negative with floating point.
+# float without sign is considered positive.
+# print(1.1, 10.11)
+# print(0.0, -1.5)
+
+# The type function shows the type that Python
+# inferred to the value.
+print(type('Igor'))
+print(type(0))
+print(type(1.1), type(-1.1), type(0.0))
