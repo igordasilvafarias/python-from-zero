22bc81ecd50d1f949dec2bf189ae0181e89314e0
Igor Farias <igordasilvafarias@gmail.com>
2026-02-03 21:48:53 -0300

    feat: add `variables.py` demonstrating basic variable assignment, types, and usage.

---
+++ class_07/variables.py
@@ -0,0 +1,18 @@
+# Variables are used to save something in the computer's memory.
+# PEP8: start variables with lowercase letters, can use
+# numbers and underscore _.
+# The = sign is the assignment operator. It is used to
+# assign a value to a name (variable).
+# Usage: variable_name = expression
+
+# full_name = 'Igor Farias'
+# sum_two_plus_two = 2 + 2
+# int_one = bool('1')
+# print(int_one, type(int_one))
+# print(full_name, sum_two_plus_two)
+
+name = 'Igor'
+age = 39
+is_of_age = age >= 18
+print('Name:', name, 'Age:', age)
+print('Is of age?', is_of_age)
