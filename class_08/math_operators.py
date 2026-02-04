c8866a3932f3d8d61634a9fc92f4635794666ec9
Igor Farias <igordasilvafarias@gmail.com>
2026-02-04 16:22:51 -0300

    feat: add `math_operators.py` demonstrating Python's arithmetic operations.

---
+++ class_08/math_operators.py
@@ -0,0 +1,26 @@
+addition = 10 + 10
+print('Addition', addition)
+
+subtraction = 10 - 5
+print('Subtraction', subtraction)
+
+multiplication = 10 * 10
+print('Multiplication', multiplication)
+
+division = 10 / 3  # float
+print('Division', division)
+
+integer_division = 10 // 3
+print('Integer division', integer_division)
+
+exponentiation = 2 ** 10
+print('Exponentiation', exponentiation)
+
+modulo = 55 % 2  # division remainder
+print('Modulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
