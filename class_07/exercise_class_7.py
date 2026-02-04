dff93e5cf00fdda943d80cd5e50ecd02f59df49a
Igor Farias <igordasilvafarias@gmail.com>
2026-02-03 21:55:08 -0300

    feat: Add `exercise_class_7.py` demonstrating variable declaration and printing of various data types.

---
+++ class_07/exercise_class_7.py
@@ -0,0 +1,13 @@
+name: str = 'Igor'
+last_name: str = 'Farias'
+age: int = 39
+year_of_birth: int = 1986
+is_of_age: bool = age >= 18
+height: float = 1.80
+
+print('Name:', name)
+print('Last Name:', last_name)
+print('Age:', age)
+print('Year of Birth:', year_of_birth)
+print('Is of age?', is_of_age)
+print('Height:', height)
